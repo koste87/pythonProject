e09e427be9acc42a516f45b81a3e72f4996f2453
koste87 <kostel8877@gmail.com>
2024-07-13 18:42:30 +1000

    Add mask_account_card function

--- src/widget.py
+++ src/widget.py
@@ -9,3 +9,12 @@ def mask_account_card(info: str) -> str:
     else:
         card_type, card_number = info.rsplit(' ', 1)
         return f"{card_type} {mask_card(card_number)}"
+
+
+# src/widget.py
+
+from datetime import datetime
+
+def get_date(date_str: str) -> str:
+    date_obj = datetime.fromisoformat(date_str)
+    return date_obj.strftime("%d.%m.%Y")
